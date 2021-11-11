69f5eba80d09105a952d6a730f463fe305ab4bd8
clearsky77 <clearsky3264@gmail.com>
2021-11-11 18:20:03 +0900

    1. 문제 출제 구현

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,39 @@
 package codes;
 
+import java.util.Random;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 		
 //		1. 문제 출제
+		int[] cpuNumbers = new int[3];
+		
+		for(int i=0; i<cpuNumbers.length; i++) {
+			while(true) {
+				// 랜덤 숫자 추출
+				int randomNum = (int)(Math.random()*9+1);
+				// 중복인지 확인
+				boolean isDuplOK = true;
+				for(int cpuNum : cpuNumbers) {
+					if(cpuNum == randomNum) {
+						isDuplOK = false;
+					}
+				}
+				
+				if(isDuplOK) {
+					cpuNumbers[i] = randomNum;
+					break;
+				}
+			}
+		}
+		
+		// 출제한 문제 값 출력
+		System.out.println("문제 숫자");
+		for(int cpuNum : cpuNumbers) {
+			System.out.print(cpuNum+" ");
+		}
+		
 		
 		
 //		2. 숫자 입력 받기

4dcab39418f7f0860726aca9fccf12ded79831ba
clearsky77 <clearsky3264@gmail.com>
2021-11-11 18:07:33 +0900

    프로젝트 기본 구조

---
+++ src/codes/MainDrive.java
@@ -0,0 +1,21 @@
+package codes;
+
+public class MainDrive {
+
+	public static void main(String[] args) {
+		
+//		1. 문제 출제
+		
+		
+//		2. 숫자 입력 받기
+		
+		
+//		3. 정답인지 판별
+		
+		
+//		4. 결과 출력
+		
+		
+	}
+
+}
